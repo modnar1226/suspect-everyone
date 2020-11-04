63a58f605106cffb6f7c1527ea0e8e25a3f54706
Ian Greene <Modnar1226@bitbucket.org>
2020-11-04 16:07:18 -0500

    removed breaking lines from index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
 //import query from '../lib/database'
 import Link from 'next/link'
-import { PrismaClient } from '@prisma/client';
 
 export default function Home({ allPostsData, result }) {
   return (
@@ -31,47 +29,3 @@ export default function Home({ allPostsData, result }) {
     </Layout>
   )
 }
-
-export async function getServerSideProps() {
-  // fetch post data, could be database calls
-  const allPostsData = getSortedPostsData()
-  /*const result = JSON.parse(JSON.stringify(await query({
-    query: 'SELECT * FROM users',
-    values: []
-  }).then(function (results) { return results })));
-  console.log(result);
-  */
-  const prisma = new PrismaClient();
-  const result = JSON.parse(JSON.stringify(await prisma.users
-    .findOne({
-      where: {email : 'isg1315122@gmail.com'}
-    })
-    .contacts()
-  ));
-  return {
-    props: {
-      allPostsData,
-      result
-    }
-  }
-}
-
-// static properties, used to preload a page
-/*
-export async function getStaticProps() {
-  // fetch post data, could be database calls
-  const allPostsData = getSortedPostsData()
-  const result = JSON.parse(JSON.stringify(await query({
-    query: 'SELECT * FROM users',
-    values: []
-  }).then(function(results){return results})));
-  console.log(result);
-  return {
-    props: {
-      allPostsData,
-      result
-    }
-  }
-}
-*/
-
